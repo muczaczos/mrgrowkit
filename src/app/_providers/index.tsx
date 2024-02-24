d6ba107425b5bb3455f7a0ff3b3de03dc2aa5a61
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-02-24 13:06:15 +0100

    I chenged CollectionArchive styles

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 import { AuthProvider } from '../_providers/Auth'
 import { CartProvider } from '../_providers/Cart'
+import { FilterProider } from './Filter'
 import { ThemeProvider } from './Theme'
 
 export const Providers: React.FC<{
@@ -12,7 +13,9 @@ export const Providers: React.FC<{
   return (
     <ThemeProvider>
       <AuthProvider>
-        <CartProvider>{children}</CartProvider>
+        <FilterProider>
+          <CartProvider>{children}</CartProvider>
+        </FilterProider>
       </AuthProvider>
     </ThemeProvider>
   )
