db8666b3bd7672255b3bc21821b6e5c45e263b07
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-02-29 15:57:15 +0100

    I corrected formatSlug

--- src/payload/hooks/formatSlug.ts
+++ src/payload/hooks/formatSlug.ts
@@ -8,20 +8,20 @@ const format = (val: string): string =>
 
 const formatSlug =
   (fallback: string): FieldHook =>
-    ({ operation, value, originalDoc, data }) => {
-      if (typeof value === 'string') {
-        return format(value)
-      }
-
-      if (operation === 'create') {
-        const fallbackData = data?.[fallback] || originalDoc?.[fallback]
-
-        if (fallbackData && typeof fallbackData === 'string') {
-          return format(fallbackData)
-        }
-      }
-
-      return value
+  ({ operation, value, originalDoc, data }) => {
+    if (typeof value === 'string') {
+      return format(value)
     }
+
+    if (operation === 'create') {
+      const fallbackData = data?.[fallback] || originalDoc?.[fallback]
+
+      if (fallbackData && typeof fallbackData === 'string') {
+        return format(fallbackData)
+      }
+    }
+
+    return value
+  }
 
 export default formatSlug
