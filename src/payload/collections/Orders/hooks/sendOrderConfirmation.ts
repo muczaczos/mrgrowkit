c80580fb7239547e6ba8346c20efddfa0da70ca4
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-03-13 15:41:42 +0100

    Send order confirmation email with proper data

--- src/payload/collections/Orders/hooks/sendOrderConfirmation.ts
+++ src/payload/collections/Orders/hooks/sendOrderConfirmation.ts
@@ -2,14 +2,17 @@ import type { AfterChangeHook } from 'payload/dist/collections/config/types'
 
 import type { Order } from '../../../payload-types'
 
-export const sendOrderConfirmation: AfterChangeHook<Order> = async ({ req }) => {
+export const sendOrderConfirmation: AfterChangeHook<Order> = async ({ req, doc }) => {
   const { payload } = req
 
   await payload.sendEmail({
-    to: 'muczaczos@gmail.com',
+    to: doc.email,
     from: 'shop@planet-of-mushrooms.com',
     subject: 'New Order',
-    html: '<b>Hey there!</b><br/>Thank you for your order!',
+    html:
+      '<b>Hey there!</b><br/>Thank you for your order! Total amount of your order is: ' +
+      doc.total +
+      '. <br/> Have a nice day facker!!!',
   })
 
   return
