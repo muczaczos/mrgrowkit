49db601ebf9f477bdcdbb96d7f16eefd0954b160
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-03-18 10:48:41 +0100

    Error when deploying (layout in posts)

--- src/app/(pages)/posts/page.tsx
+++ src/app/(pages)/posts/page.tsx
@@ -37,7 +37,7 @@ const Posts = async () => {
   return (
     <div className={classes.container}>
       <Gutter className={classes.products}>
-        <h1>madafaka posts</h1>
+        <Blocks blocks={page.layout} disableTopPadding={true} />
       </Gutter>
       <HR />
     </div>
