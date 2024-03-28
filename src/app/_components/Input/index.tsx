cf55851ad506a25df1a62e1ec5bda56e48cc9ccf
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-03-28 20:54:48 +0100

    npm run lint:fix

--- src/app/_components/Input/index.tsx
+++ src/app/_components/Input/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { FieldValues, UseFormRegister } from 'react-hook-form';
+import React from 'react'
+import { FieldValues, UseFormRegister } from 'react-hook-form'
 
-import classes from './index.module.scss';
+import classes from './index.module.scss'
 
 type Props = {
-  name: string;
-  label: string;
-  register: UseFormRegister<FieldValues & any>;
-  required?: boolean;
-  error: any;
-  type?: 'text' | 'number' | 'password' | 'email' | 'textarea';
-  validate?: (value: string) => boolean | string;
-  placeholder?: string;
-  disabled?: boolean;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  className?: string;
-};
+  name: string
+  label: string
+  register: UseFormRegister<FieldValues & any>
+  required?: boolean
+  error: any
+  type?: 'text' | 'number' | 'password' | 'email' | 'textarea'
+  validate?: (value: string) => boolean | string
+  placeholder?: string
+  disabled?: boolean
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  className?: string
+}
 
 export const Input: React.FC<Props> = ({
   name,
@@ -32,9 +32,9 @@ export const Input: React.FC<Props> = ({
 }) => {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      e.preventDefault(); // Zignoruj standardowe zachowanie klawisza Enter
+      e.preventDefault() // Zignoruj standardowe zachowanie klawisza Enter
     }
-  };
+  }
   return (
     <div className={classes.inputWrap}>
       <label htmlFor={name} className={classes.label}>
@@ -69,5 +69,5 @@ export const Input: React.FC<Props> = ({
         </div>
       )}
     </div>
-  );
-};
+  )
+}
