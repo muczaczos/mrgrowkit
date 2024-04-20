fa90187d3867214560915e6801a6b46574bbe10f
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-04-20 12:09:13 +0200

    I corrected content in RichText in product. Now without error.

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -17,7 +17,26 @@ export const ProductHero: React.FC<{
   product: Product
 }> = ({ product }) => {
   const { title, categories, price, layout, meta: { image: metaImage, description } = {} } = product
+  let productDescription
+  let productDetails
+  let productFaq
 
+  // Sprawdź, czy istnieje layout w produkcie, aby uniknąć błędów w czasie wykonania
+  if (product.layout) {
+    // Iteruj przez każdy element w layout
+    product.layout.forEach(layoutItem => {
+      // Sprawdź, czy obiekt layoutItem ma właściwość columns
+      if (layoutItem.blockType === 'content' && layoutItem.columns) {
+        // Jeśli columns istnieje, możesz uzyskać do niego dostęp tutaj
+        const columns = layoutItem.columns
+        // Możesz dalej przetwarzać columns
+        // np. możesz iterować przez każdą kolumnę i wykonywać odpowiednie operacje
+        productDescription = columns[0]
+        productDetails = columns[1]
+        productFaq = columns[2]
+      }
+    })
+  }
   return (
     <Gutter className={classes.productHero}>
       <div className={classes.mediaWrapper}>
@@ -83,7 +102,7 @@ export const ProductHero: React.FC<{
                       'rgba(0, 0, 0, 0.3) rgba(0, 0, 0, 0)' /* Kolor paska przewijania */,
                   }}
                 >
-                  <RichText content={layout[0].columns[0].richText} />
+                  <RichText content={productDescription.richText} />
                 </div>
               </TabPanel>
               <TabPanel>
@@ -99,7 +118,7 @@ export const ProductHero: React.FC<{
                       'rgba(0, 0, 0, 0.3) rgba(0, 0, 0, 0)' /* Kolor paska przewijania */,
                   }}
                 >
-                  <RichText content={layout[0].columns[1].richText} />
+                  <RichText content={productDetails.richText} />
                 </div>
               </TabPanel>
               <TabPanel>
@@ -115,7 +134,7 @@ export const ProductHero: React.FC<{
                       'rgba(0, 0, 0, 0.3) rgba(0, 0, 0, 0)' /* Kolor paska przewijania */,
                   }}
                 >
-                  <RichText content={layout[0].columns[2].richText} />
+                  <RichText content={productFaq.richText} />
                 </div>
               </TabPanel>
               <TabPanel>
@@ -131,7 +150,7 @@ export const ProductHero: React.FC<{
                       'rgba(0, 0, 0, 0.3) rgba(0, 0, 0, 0)' /* Kolor paska przewijania */,
                   }}
                 >
-                  <RichText content={layout[0].columns[2].richText} />
+                  <RichText content={productFaq.richText} />
                 </div>
               </TabPanel>
             </div>
