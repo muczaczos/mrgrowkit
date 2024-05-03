3d384430708042e72a1ddeeae24b3a28ea5846be
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-05-03 22:01:50 +0200

    I added contact form to contact page

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { BsFacebook, BsInstagram, BsTwitter } from 'react-icons/bs'
 import { Metadata } from 'next'
 import Image from 'next/image'
@@ -11,21 +11,57 @@ import { Gutter } from '../../_components/Gutter'
 import { generateMeta } from '../../_utilities/generateMeta'
 
 import classes from './index.module.scss'
+import { Input } from '../../_components/Input'
+import { useForm } from 'react-hook-form'
+import RichText from '../../_components/RichText'
 
 // Force this page to be dynamic so that Next.js does not cache it
 // See the note in '../[slug]/page.tsx' about this
 export const dynamic = 'force-dynamic'
 
 export default function Contact() {
+
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [message, setMessage] = useState('')
+  
+  const handleChange = event => {
+    // console.log(event.target.value)
+    setMessage(event.target.value)
+  }
+
   const handleWhatsAppClick = () => {
     // Twój numer telefonu WhatsApp
-    const phoneNumber = '1234567890'
+    const phoneNumber = '+48691586665'
     // Twój wiadomość, którą chcesz wysłać
     const message = 'Cześć! Jestem zainteresowany Twoją ofertą.'
     // Tworzymy link do WhatsApp z numerem telefonu i wiadomością
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`
     // Przekierowanie użytkownika do aplikacji WhatsApp
     window.location.href = whatsappUrl
+  }
+
+  const handleCallClick = () => {
+    // Twój numer telefonu
+    const phoneNumber = '+48691586665';
+    // Tworzymy link do wybierania połączenia na telefonie
+    const callUrl = `tel:${phoneNumber}`;
+    // Przekierowanie użytkownika do wybierania połączenia
+    window.location.href = callUrl;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isLoading },
+  } = useForm<FormData>()
+
+  const handleName = e => {
+    setName(e.target.value)
+  }
+
+  const handleEmail = e => {
+    setEmail(e.target.value)
   }
 
   return (
@@ -52,7 +88,7 @@ export default function Contact() {
           <h4 className="text-right">VatUE: PL7971643224</h4>
 
           <section className="flex flex-col lg:flex-row gap-5 mt-10 mb-16">
-            <div className="bg-gray-50 rounded-xl p-6 shadow-xl mb-5">
+            <div className="flex flex-col items-center bg-gray-50 rounded-xl p-6 shadow-xl mb-5">
               <Image
                 src="/media/Jan.svg"
                 alt="James is avatar"
@@ -62,26 +98,19 @@ export default function Contact() {
               />
               <h3>James</h3>
               <h4>Mushrooms Grower</h4>
-              <p>Chat with Whatsapp</p>
-              <Button onClick={handleWhatsAppClick}>Skontaktuj się przez WhatsApp</Button>
+              <h5 className="text-center">Contact via WhatsApp.</h5>
+              <h6 className="text-center">7days / week (8am - 8pm) GMT+01:00</h6>
+              <Button onClick={handleWhatsAppClick}>
+                <Image
+                  src="/media/WhatsApp.svg"
+                  alt="Whatsapp logo"
+                  width={150}
+                  height={150}
+                  className="opacity-70 mb-5"
+                />
+              </Button>
             </div>
-            <div className="bg-gray-50 rounded-xl p-6 shadow-xl mb-5">
-              <Image
-                src="/media/Adiva.svg"
-                alt="Adiva is avatar"
-                width={200}
-                height={200}
-                className="opacity-70 mb-5"
-              />
-              <h3>Adiva</h3>
-              <h4>Help Desk</h4>
-              <p>
-                My name is Adiva and I am waiting for your e-mail. You can talk to me in English or
-                Polish. I will be happy to answer any questions about our store. My mission is to
-                help people solve their problems.
-              </p>
-            </div>
-            <div className="bg-gray-50 rounded-xl p-6 shadow-xl mb-5">
+            <div className="flex flex-col items-center bg-gray-50 rounded-xl p-6 shadow-xl mb-5">
               <Image
                 src="/media/arek.svg"
                 alt="Arek is avatar"
@@ -91,13 +120,81 @@ export default function Contact() {
               />
               <h3>Arek</h3>
               <h4>IT Technican</h4>
-              <p>
-                Hi, I’m Arek and apart from mushrooms I love computers. If you have any problems
-                with payment, the functioning of our website or the shipment, just give me a call. I
-                will try to help you.
-              </p>
+              <h5 className="text-center">Contact via Mobile</h5>
+              <h6 className="text-center">Monday - Friday (4pm - 8pm) GMT+01:00</h6>
+              <Button onClick={handleCallClick}>
+                <Image
+                  src="/media/mobile.svg"
+                  alt="Telephone logo"
+                  width={120}
+                  height={120}
+                  className="opacity-70 mb-5"
+                />
+              </Button>
             </div>
+            <div className="flex flex-col items-center bg-gray-50 rounded-xl p-6 shadow-xl mb-5">
+              <Image
+                src="/media/Adiva.svg"
+                alt="Adiva is avatar"
+                width={200}
+                height={200}
+                className="opacity-70 mb-5"
+              />
+              <h3>Adiva</h3>
+              <h4>Help Desk</h4>
+              <h5 className="text-center">Contact via email</h5>
+              <h6 className="text-center">7days / week (8am - 8pm) GMT+01:00</h6>
+              <Button href="#mail">
+                <Image
+                  src="/media/envelope.svg"
+                  alt="Envelope"
+                  width={150}
+                  height={150}
+                  className="opacity-70 mb-5"
+                />
+              </Button>
+            </div>
+            
           </section>
+        </div>
+        <div id="mail" className="rounded-xl shadow-xl bg-pink-50 p-5">
+          <div>
+            <h3 className="mb-3">Contact Form</h3>
+            <div>
+                <Input
+                  name="name"
+                  type="text"
+                  label="Name"
+                  register={register}
+                  error={null}
+                  disabled={false}
+                  value={name}
+                  onChange={handleName}
+                  className="mb-5 bg-white"
+                />
+                <Input
+                  name="email"
+                  type="email"
+                  label="Email"
+                  register={register}
+                  error={null}
+                  value={email}
+                  onChange={handleEmail}
+                  className="bg-white"
+                />
+                <p className="mt-3 mb-2">Message</p>
+                <textarea
+                  className="w-full rounded-xl"
+                  onChange={handleChange}
+                  name="mailContent"
+                  rows={15}
+                  cols={40}
+                />
+                <Button className="mt-5 p-5 w-full bg-black" onClick={handleCallClick}>
+                  <p className="text-white text-2xl">Send a message</p>
+                </Button>
+            </div>
+          </div>    
         </div>
       </Gutter>
     </div>
