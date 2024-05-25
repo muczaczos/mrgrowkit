350cad9478535d1721b03ca7a6e07991174d802c
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-05-25 14:32:02 +0200

    I changed inclusions text

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,22 +1,22 @@
 export const inclusions = [
   {
-    title: 'Free Shipping',
-    description: 'Free shipping for order above $150',
+    title: 'Discreet Shipping',
+    description: 'We add labels of edible strains',
     icon: '/assets/icons/shipping.svg',
   },
   {
-    title: 'Money Guarantee',
-    description: 'Within 30 days for an exchange',
+    title: 'Very attractive prices...',
+    description: '...and high-quality products',
     icon: '/assets/icons/dollar.svg',
   },
   {
     title: 'Online Support',
-    description: '24 hours a day, 7 days a week',
+    description: '12 hours a day, 7 days a week',
     icon: '/assets/icons/support.svg',
   },
   {
     title: 'Flexible Payment',
-    description: 'Pay with multiple credit cards',
+    description: 'Pay with multiple payment options',
     icon: '/assets/icons/payment.svg',
   },
 ]
