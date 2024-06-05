7fb17768849678d10e74600c03fa847f3e9b0161
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-06-05 15:35:05 +0200

    I've corrected server config for nginx proxy

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000
 
 // set trust proxy to true if you use nginx
 // when NodeJS app are served behind nginx reverse proxies and similar.
-app.set('trust proxy', true)
+app.set('trustProxy', true)
 
 // Dodaj middleware, który odczyta dane JSON z ciała żądania
 app.use(express.json())
