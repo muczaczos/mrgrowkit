ee46fe5a8773c2adf6c7bc737210709edca78de0
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-06-09 17:40:30 +0200

    I added metadata to cubensis-spores-syringes website

--- src/app/(pages)/cubensis-spore-syringes/page.tsx
+++ src/app/(pages)/cubensis-spore-syringes/page.tsx
@@ -1,7 +1,7 @@
-'use client'
 import React from 'react'
 import { draftMode } from 'next/headers'
 import Image from 'next/image'
+import { Metadata } from 'next'
 
 import { Category, Page, Product } from '../../../payload/payload-types'
 import { fetchDoc } from '../../_api/fetchDoc'
@@ -13,6 +13,7 @@ import SyringesCards from './SyringesCards'
 
 import classes from './index.module.scss'
 import Head from 'next/head'
+import { title } from 'process'
 
 const Syringes = async () => {
   const { isEnabled: isDraftMode } = draftMode()
@@ -42,31 +43,6 @@ const Syringes = async () => {
 
   return (
     <>
-      <Head>
-        {/* 60 char */}
-        <title>Cubensis Spore Syringes: Premium Cultivation Essentials</title>
-        {/* 150 char */}
-        <meta
-          name="description"
-          content="Unlock the mysteries of the fungal world with our premium Cubensis Spore Syringes. Perfect for both beginners and experienced cultivators"
-        />
-        <meta
-          name="keywords"
-          content="Cubensis spore syringe, Cubensis spore print, magic mushrooms, Cubensis spores, Fungi, Organic Mushrooms"
-        />
-        <meta name="author" content="www.planet-of-mushrooms" />
-        <meta
-          property="og:title"
-          content="Cubensis Spore Syringes: Premium Cultivation Essentials"
-        />
-        <meta
-          property="og:description"
-          content="Unlock the mysteries of the fungal world with our premium Cubensis Spore Syringes. Perfect for both beginners and experienced cultivators"
-        />
-        <meta property="og:image" content="/media/syringes.webp" />
-        <meta property="og:url" content="https://planet-of-mushrooms.com/cubensis-spore-syringes" />
-        <meta property="og:type" content="website" />
-      </Head>
       <Gutter>
         <div className={classes.imageTitle}>
           <Image alt="Planet of mushrooms" src="/media/growkit.jpeg" height="500" width="600" />
@@ -88,6 +64,19 @@ const Syringes = async () => {
       </Gutter>
     </>
   )
+}
+
+// either Static metadata
+export const metadata: Metadata = {
+  title: 'Cubensis Spore Syringes: Premium Cultivation Essentials', //60 char
+  description: 'Unlock the mysteries of the fungal world with our premium Cubensis Spore Syringes. Perfect for both beginners and experienced cultivators', //150 char
+  keywords: 'Cubensis spore syringe, Cubensis spore print, magic mushrooms, Cubensis spores, Fungi, Organic Mushrooms',
+  openGraph: {
+    images: ['/media/growkit.jpeg'],
+    title: 'Cubensis Spore Syringes: Premium Cultivation Essentials',
+    url: 'https://planet-of-mushrooms.com/cubensis-spore-syringes',
+    type: 'website'
+  }
 }
 
 export default Syringes
