437aff2513d47c7527f1cb6f6c991d5b6a789bc0
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-06-18 22:08:46 +0200

    I created static, dynamic sitemap.xml

--- src/app/connectMongo.js
+++ src/app/connectMongo.js
@@ -10,14 +10,14 @@ export async function connectToDatabase() {
   }
 
   if (!MONGODB_URI) {
-    console.log('MongoDB URI not found')
+    // console.log('MongoDB URI not found')
   }
 
   try {
     client = await MongoClient.connect(MONGODB_URI)
-    console.log('Connected to MongoDB successfuly')
+    // console.log('Connected to MongoDB successfuly')
     return client
   } catch (error) {
-    console.log('Error connecting to the database: ', error)
+    // console.log('Error connecting to the database: ', error)
   }
 }
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,27 +3,76 @@ import { connectToDatabase } from './connectMongo'
 export default async function sitemap() {
   const client = await connectToDatabase()
   const db = client.db('planet2')
-  const data = await db.collection('posts').find({}).toArray()
+  const postsData = await db.collection('posts').find({}).toArray()
+  const pagesData = await db.collection('pages').find({}).toArray()
+  const productsData = await db.collection('products').find({}).toArray()
 
-  console.log(data)
+  const posts = postsData.map(item => ({
+    url: `${process.env.NEXT_PUBLIC_SERVER_URL}/posts/${item.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.5,
+  }))
+
+  const pages = pagesData.map(item => ({
+    url: `${process.env.NEXT_PUBLIC_SERVER_URL}/${item.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.5,
+  }))
+
+  const products = productsData.map(item => ({
+    url: `${process.env.NEXT_PUBLIC_SERVER_URL}/${item.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.5,
+  }))
+
   return [
     {
-      url: 'http://localhost:3000/',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: 'https://acme.com/about',
+      url: process.env.NEXT_PUBLIC_SERVER_URL,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://acme.com/blog',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
       priority: 0.5,
     },
+    {
+      url: `${process.env.NEXT_PUBLIC_SERVER_URL}/Cubensis-grow-kits`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SERVER_URL}/Cubensis-spore-syringes`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cubensis-liquid-cultures`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cubensis-spore-prints`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cubensis-plate-cultures`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SERVER_URL}/contact`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    ...pages,
+    ...posts,
+    ...products,
   ]
 }
