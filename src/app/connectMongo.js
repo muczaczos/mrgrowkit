b8b9d2b92b6f35f4fe32bb9a73f40a73f16ba56b
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-06-18 15:49:57 +0200

    I connected to db from sitemap

---
+++ src/app/connectMongo.js
@@ -0,0 +1,23 @@
+import { MongoClient } from 'mongodb'
+
+const MONGODB_URI = process.env.DATABASE_URI
+
+let client = null
+
+export async function connectToDatabase() {
+  if (client) {
+    return client
+  }
+
+  if (!MONGODB_URI) {
+    console.log('MongoDB URI not found')
+  }
+
+  try {
+    client = await MongoClient.connect(MONGODB_URI)
+    console.log('Connected to MongoDB successfuly')
+    return client
+  } catch (error) {
+    console.log('Error connecting to the database: ', error)
+  }
+}
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,4 +1,11 @@
-export default function sitemap() {
+import { connectToDatabase } from './connectMongo'
+
+export default async function sitemap() {
+  const client = await connectToDatabase()
+  const db = client.db('planet2')
+  const data = await db.collection('posts').find({}).toArray()
+
+  console.log(data)
   return [
     {
       url: 'http://localhost:3000/',
